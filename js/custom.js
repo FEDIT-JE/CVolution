c94acad0a238b50a56fdf42628819bdf0a2532d5
Jan Eggenberger <j.eggenberger@federerag.ch>
2024-12-30 16:30:39 +0100

    change

--- js/custom.js
+++ js/custom.js
@@ -44,3 +44,49 @@ function myMap() {
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById("countdownModal");
+    var span = document.getElementsByClassName("close")[0];
+    var daysElement = document.getElementById("days");
+    var hoursElement = document.getElementById("hours");
+    var minutesElement = document.getElementById("minutes");
+    var secondsElement = document.getElementById("seconds");
+
+    var targetDate = new Date("July 1, 2025 00:00:00").getTime();
+
+    modal.style.display = "block";
+
+    var countdownInterval = setInterval(function() {
+        var now = new Date().getTime();
+        var distance = targetDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        daysElement.textContent = days;
+        hoursElement.textContent = hours;
+        minutesElement.textContent = minutes;
+        secondsElement.textContent = seconds;
+
+        if (distance < 0) {
+            clearInterval(countdownInterval);
+            modal.style.display = "none";
+        }
+    }, 1000);
+
+    span.onclick = function() {
+        modal.style.display = "none";
+        clearInterval(countdownInterval);
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            clearInterval(countdownInterval);
+        }
+    }
+});
+  
